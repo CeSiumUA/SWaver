6241514f5aae257b9d994663b4b18ee30034a7de
Fedir Katushonok <mtg.in.ua@gmail.com>
2021-05-23 20:37:38 +0300

    removed js files

--- src/environments/dist/environment.prod.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.environment = void 0;
-exports.environment = {
-    tab: 0,
-    production: true
-};
--- src/math/dist/fifthLabComponent.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.FifthLabCalculation = void 0;
-var FifthLabCalculation = /** @class */ (function () {
-    function FifthLabCalculation() {
-    }
-    return FifthLabCalculation;
-}());
-exports.FifthLabCalculation = FifthLabCalculation;
